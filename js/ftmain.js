c524a4dda2ae58d7c93568a2bf6021a29ffa7695
peayah <82358003+peayah@users.noreply.github.com>
2022-02-07 01:40:46 -0800

    initial commit

---
+++ js/ftmain.js
@@ -0,0 +1,30 @@
+function myfunction(obj) {
+  if (obj.checked) {
+    // console.log("if obj.checked")
+    // console.log(obj.id)
+    // console.log(obj.checked)
+    localStorage.setItem(obj.id, obj.checked)
+    // location.reload()
+  } else {
+    // console.log("if not obj.checked")
+    // console.log(obj.id)
+    // console.log(obj.checked)
+    localStorage.removeItem(obj.id);
+    // location.reload()
+  }
+}
+
+function loadCheck() {
+  var list = document.querySelectorAll(`[type*="checkbox"]`);
+  list.forEach(el => {
+    if (localStorage.getItem(el.id) === "true") {
+      document.getElementById(el.id).checked = true
+      console.log("this should work")
+    }
+  })
+}
+
+function uncheckAll() {
+  document.querySelectorAll('input[type="checkbox"]')
+    .forEach(el => el.checked = false);
+}
